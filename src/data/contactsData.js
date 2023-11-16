b522255757b22becd95ed06e6877ac73d6d2f6cf
Gulsum Keskin <keskingulsum313@gmail.com>
2023-11-16 19:29:01 +0000

    first commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'keskingulsum313@gmail.com',
+    phone: '+44741959631',
+    address: 'Wandsworth, London, United Kingdom - SW11 3PP ',
+
+    sheetAPI: ''
+}
---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from '../assets/pdf/Gulsum.pdf'
+
+export const headerData = {
+  name: "Gulsum Keskin",
+  title: "Web Developer",
+  desciption:"Motivated Junior React Developer with a strong dedication to creating dynamic and user-friendly interfaces. Seeking to apply expertise in ReactJS, JavaScript, HTML, and CSS to drive innovation in project development. Enthusiastic about embracing new technologies, fostering collaboration in team settings, and introducing innovative approaches to optimize development workflows. Committed to crafting intuitive, high-performance web applications while consistently enhancing skills and keeping up with evolving industry trends.",
+    image:
+    "https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png",
+  resumePdf: resume,
+};
